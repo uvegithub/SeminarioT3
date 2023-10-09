4c37d61365b5a15047df099583203a202d07b73e
uvegithub <vanesanunez90dos@gmail.com>
2023-10-09 20:54:01 +0200

    masEjer

--- app/src/main/java/com/example/act_t3_2_10/SeminarioT3.kt
+++ app/src/main/java/com/example/act_t3_2_10/SeminarioT3.kt
@@ -42,7 +42,50 @@ fun palin(cadena:String):Boolean{
     return res
 }
 
+/** Ejercicio 5. Crea una función que cuenta cuántas veces aparece una letra en un
+texto. */
+fun repLet(cadena:String, letra:Char):Int{
+    var res: Int=0
+    for(i in 0 until cadena.length){
+        if(cadena[i]==letra){
+            res++
+        }
+    }
+    return res
+}
+
+/** Ejercicio 6. Crea una función que cuenta cuántas veces aparece una subcadena en
+un texto. */
+fun subca(texto:String, subcadena:String):Int{
+    var cont:Int=0
+    for(i in 0 until texto.length){
+        var char: Char=texto[i]
+        var charcad: Char=subcadena[i]
+        if(char==charcad){
+            cont++
+        }
+    }
+    return cont
+}
+
+/** Ejercicio 7. Crea una función que pone en mayúscula la primera letra de cada
+palabra de un texto. */
+fun mayus(texto:String):String{
+    var array=texto.split(" ")
+    var res: String=""
+    for(i in 0 .. array.size-1){
+        if(i==1){
+            array[i].uppercase()
+        }
+        res+=array[i]
+    }
+    return res
+}
+
 fun main(){
 /** 4 */
-    print(palin("amar"))
+    println(palin("amar"))
+
+    /** 5 */
+    println(repLet("amar", 'a'))
 }
